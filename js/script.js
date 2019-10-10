2b4510c69feb24a5e91c1a9bfb8686a397cdb1ed
Sebastian Malcher <sebastianlukasmalcher@gmail.com>
2019-10-10 10:57:42 +0200

    adds products

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
 
+let cars = 
 
-
-
-let cars = {
-
-    "car": [
+    [
     {
 
         "name": "Tesla",
@@ -90,7 +87,7 @@ let cars = {
 
         "image": "./img/tesla.jpg",
 
-        "price": "42000"
+        "price": "50000"
 
     },
     {
@@ -101,19 +98,46 @@ let cars = {
 
         "image": "./img/tesla.jpg",
 
-        "price": "20000"
-
-    },
-    {
-
-        "name": "Tesla",
-
-        "productionYear": "2000",
-
-        "image": "./img/tesla.jpg",
-
-        "price": "10000"
+        "price": "50000"
 
     }]
+
+    
+
+createContent();
+
+
+function createContent(){
+    let products = document.getElementById("products");
+    for (i = 0; i < cars.length; i++){
+        let container = document.createElement("div");
+        let img = document.createElement("img");
+        let description = document.createElement("div");
+        container.className = "container";
+        container.id = i;
+        img.src = cars[i].image;
+        description.innerText = cars[i].name;
+        container.appendChild(img);
+        container.appendChild(description);
+        products.appendChild(container);
+
+    }
+    
+
+    /*for (let car of cars){
+        let container = document.createElement("div");
+        let img = document.createElement("img");
+        let description = document.createElement("div");
+        container.className = "container";
+        img.src = car.image;
+        description.innerText = car.name;
+        container.appendChild(img);
+        container.appendChild(description);
+        products.appendChild(container);
+    }*/
+
+}
+
+function createBiggerContent(){
 
 }
