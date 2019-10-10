5ef47cfd7a1be027b2aa630b50692a6b2618a8c0
Sebastian Malcher <sebastianlukasmalcher@gmail.com>
2019-10-10 10:00:12 +0200

    creates data.json, and js car object

---
+++ script.js
@@ -0,0 +1,119 @@
+
+
+
+
+let cars = {
+
+    "car": [
+    {
+
+        "name": "Tesla",
+
+        "productionYear": "2000",
+
+        "image": "./img/tesla.jpg",
+
+        "price": "40000"
+
+    },
+    {
+
+        "name": "Tesla",
+
+        "productionYear": "2000",
+
+        "image": "./img/tesla.jpg",
+
+        "price": "30000"
+
+    },
+    {
+
+        "name": "Tesla",
+
+        "productionYear": "2000",
+
+        "image": "./img/tesla.jpg",
+
+        "price": "20000"
+
+    },
+    {
+
+        "name": "Tesla",
+
+        "productionYear": "2000",
+
+        "image": "./img/tesla.jpg",
+
+        "price": "25000"
+
+    },
+    {
+
+        "name": "Tesla",
+
+        "productionYear": "2000",
+
+        "image": "./img/tesla.jpg",
+
+        "price": "15000"
+
+    },
+    {
+
+        "name": "Tesla",
+
+        "productionYear": "2000",
+
+        "image": "./img/tesla.jpg",
+
+        "price": "30000"
+
+    },
+    {
+
+        "name": "Tesla",
+
+        "productionYear": "2000",
+
+        "image": "./img/tesla.jpg",
+
+        "price": "50000"
+
+    },
+    {
+
+        "name": "Tesla",
+
+        "productionYear": "2000",
+
+        "image": "./img/tesla.jpg",
+
+        "price": "42000"
+
+    },
+    {
+
+        "name": "Tesla",
+
+        "productionYear": "2000",
+
+        "image": "./img/tesla.jpg",
+
+        "price": "20000"
+
+    },
+    {
+
+        "name": "Tesla",
+
+        "productionYear": "2000",
+
+        "image": "./img/tesla.jpg",
+
+        "price": "10000"
+
+    }]
+
+}
